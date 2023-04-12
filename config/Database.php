fd64d49ffe297222ad5518d3883b6f1981460240
loctvl842 <loclepnvx@gmail.com>
2023-04-12 21:14:09 +0700

    fix: problem with utf8_decode

--- config/Database.php
+++ config/Database.php
@@ -3,16 +3,16 @@ class Database
 {
   // mysql://b180c081dcb982:6fbb298e@us-cdbr-east-06.cleardb.net/heroku_a0e4ed6b731d7f0?reconnect=true
   // deployed info
-  // private $host = "54.179.5.227";
-  // private $db_name = "clickzone";
-  // private $username = "loc";
-  // private $password = "Thangcho1234$";
+  private $host = "54.179.5.227";
+  private $db_name = "clickzone";
+  private $username = "loc";
+  private $password = "Thangcho1234$";
 
   // dev info
-  private $host = "localhost";
-  private $db_name = "click_zone";
-  private $username = "mysql";
-  private $password = "thangcho";
+  // private $host = "localhost";
+  // private $db_name = "click_zone";
+  // private $username = "mysql";
+  // private $password = "thangcho";
 
   private $conn;
 
@@ -20,7 +20,15 @@ class Database
   {
     $this->conn = null;
     try {
-      $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+      $this->conn = new PDO(
+        "mysql:host=$this->host;dbname=$this->db_name",
+        $this->username,
+        $this->password,
+        array(
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+          PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        )
+      );
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $err) {
       echo "Connection Error: " . $err->getMessage();
