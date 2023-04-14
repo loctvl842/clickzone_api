8f9c9cce2c3dd7faa00bb5a204f6fd55b87f1d05
loctvl842 <loclepnvx@gmail.com>
2023-04-14 21:39:07 +0700

    feat: add temp api to get hot products

---
+++ api/product/get_hot_items.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+include_once '../../config/Database.php';
+include_once '../../models/Product.php';
+
+$database = new Database();
+$conn = $database->connect();
+
+$product = new Product($conn);
+
+try {
+  $products = $product->getBy_pageNumber(0, 5);
+  http_response_code(200);
+  echo json_encode(array(
+    "success" => true,
+    "products" => $products,
+  ));
+} catch (Exception $e) {
+  http_response_code(500);
+  echo json_encode(array(
+    "success" => false,
+    "message" => $e->getMessage(),
+  ));
+}
